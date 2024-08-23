a0e9d9225251144c0fa37b31663feb319e2584fe
Szymon Lipka <szymonlipkaa@gmail.com>
2024-08-23 23:58:42 +0200

    fix lint

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  root 'welcome#index'
+  root "welcome#index"
 end
