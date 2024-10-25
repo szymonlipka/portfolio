556b7daaa657f1c73725a6ededaf013a3fa9fd50
Szymon Lipka <szymonlipkaa@gmail.com>
2024-10-25 22:42:41 +0200

    Improve navbar, change photo and some tweaks

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['"Proxima Nova"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
